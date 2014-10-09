32b8fedfa6f6d72baa20619fd1d2c7f277b38c7d
James Harris <james.harris.dkac@statefarm.com>
2014-10-08 20:24:22 -0700

    Added endpoints

---
+++ Riot_CDN_Endpoints.js
@@ -0,0 +1,7 @@
+var versionNumber = "4.17.1/";
+var baseLeagueCDN = "http://ddragon.leagueoflegends.com/cdn/";
+var imgEndPoint = baseLeagueCDN + versionNumber + "img/";
+var championPicturesEndpoint = imgEndPoint + "champion/"; //Pngs
+var passivePicturesEndPoint = imgEndPoint + "passive/"; //Pngs
+var skillsPicturesEndPoint = imgEndPoint + "spell/"; //Pngs
+var skinSplashEndPoint = baseLeagueCDN + "img/champion/splash/"; //Jpgs
