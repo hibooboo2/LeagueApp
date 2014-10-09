0474d03e55c4f697775f2f1be8ba189b69ff8466
= <=>
2014-10-08 23:53:05 -0700

    Added some more endpoints and refactored naming scheme

--- Riot_CDN_Endpoints.js
+++ Riot_CDN_Endpoints.js
@@ -2,12 +2,12 @@ var endPoints = {};
 module.exports = endPoints;
 endPoints.versionNumber = "4.17.1/";
 endPoints.baseLeagueCDN = "http://ddragon.leagueoflegends.com/cdn/";
-endPoints.imgEndPoint = endPoints.baseLeagueCDN + endPoints.versionNumber + "img/";
-endPoints.championPicturesEndpoint = endPoints.imgEndPoint + "champion/"; //Pngs
-endPoints.passivePicturesEndPoint = endPoints.imgEndPoint + "passive/"; //Pngs
-endPoints.skillsPicturesEndPoint = endPoints.imgEndPoint + "spell/"; //Pngs
-endPoints.skinSplashEndPoint = endPoints.baseLeagueCDN + "img/champion/splash/"; //Jpgs
-
+endPoints.img = endPoints.baseLeagueCDN + endPoints.versionNumber + "img/";
+endPoints.championPictures = endPoints.img + "champion/"; //Pngs
+endPoints.passivePictures = endPoints.img + "passive/"; //Pngs
+endPoints.skillsPictures = endPoints.img + "spell/"; //Pngs
+endPoints.skinSplash = endPoints.baseLeagueCDN + "img/champion/splash/"; //Jpgs
+endPoints.itemImages = endPoints.baseLeagueCDN + endPoints.versionNumber + "img/item/";
 endPoints.getChampSplash = function () {
 
 };
